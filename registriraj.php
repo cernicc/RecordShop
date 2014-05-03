c0036677633da8bb7d185fcd8755f7465a5deca1
z-a-x <plut.jaka@gmail.com>
2014-05-03 19:15:44 +0200

    Popravilo registracije
    Dodal sem, da se ne da oddajat nepopolne registracije.
    Preden submitaš, preveri, ali so polja vnešena.
    Če polja niso vnešena, vsa ki so - prepiše, ostalim doda opozorilo.
    Izjema je geslo, ki ga je vedno potrebno na novo vnesti.
    
    Na tem mestu me zanima, kaj je s prijavo. Meni namreč ne dela.
    Rok?

--- registriraj.php
+++ registriraj.php
@@ -11,6 +11,11 @@
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+
+
+
+
+
 	</head>
 	<body>
 		<div class="wrap"> <!---start-wrap--->
@@ -21,6 +26,12 @@
 						<div class="col span_2_of_3">
 				  			<div class="contact-form">
 								<?php
+                                $username = "";
+                                $email = "";
+                                $password = "";
+
+                                $name = "";
+                                $surname = "";
 								// if already logged in redirect to index
 								if (isset($_SESSION['username']))
 								{
@@ -28,6 +39,7 @@
 									die();
 								  }
 								$userExists = false;
+
 								if (isset($_POST['register'])){
 																
 									$dbname = 'recordshop'; 
@@ -57,8 +69,9 @@
 									}
 		
 									if (mysql_num_rows($query) > 0) {
-										$userExists = true;								
+										$userExists = true;
 									}
+
 									
 									
 									/*
@@ -83,24 +96,100 @@
 										$password = $_POST['password'];
 										$password = hash('sha512', $password);
 										$name = $_POST['name'];
-										$surname = $_POST['surname'];		
-										$sqlInsert = "INSERT INTO uporabnik (uporabnisko_ime, geslo, ime, priimek, email)
-														VALUES ('$username', '$password', '$name', '$surname', '$email');";
-										// execute sqlInsert
-										$sqlSumniki = "SET NAMES 'utf8' COLLATE 'utf8_slovenian_ci';";
-										$sqlUTF = "SET CHARACTER SET 'utf8_slovenian_ci';";
-										mysql_query($sqlUTF);
-										mysql_query($sqlSumniki);
-										mysql_query($sqlInsert);
-										//mysqli_query($dbConn, $sqlInsert);
-										echo "Uporabnik $username uspešno registriran!";
-										echo "</br>";
-										echo "Čez par sekund boste preusmerjeni na stran za prijavo!";
-										header("refresh:5; url=index.php");
-		
+										$surname = $_POST['surname'];
+
+
+                                        if($username =="" || $email =="" || $password =="" || $name =="" || $surname =="" ){
+                                            echo "<h3>Registracija uporabnika</h3>";
+                                            echo "<form id='registerForm' name='registerForm' action='registriraj.php' method='POST'>";
+                                            echo "<div>";
+                                            echo "<span><label>IME</label></span>";
+
+                                            if($name==""){
+                                                echo "<span><input type='text' name='name' id='txtName'><span>";
+                                                echo "<p1>* vpišite ime!</p1>";
+                                            }
+                                            else{
+                                                echo "<span><input type='text' name='name' id='txtName' value='$name'><span>";
+                                            }
+
+                                            echo "</div>";
+                                            echo "<div>";
+                                            echo "<span><label>PRIIMEK</label></span>";
+
+                                            if($surname==""){
+                                                echo "<span><input type='text' name='surname' id='txtSurname'></span>";
+                                                echo "<p1>* vpišite priimek!</p1>";
+                                            }
+                                            else{
+                                                echo "<span><input type='text' name='surname' id='txtName' value='$name'><span>";
+                                            }
+                                            echo "</div>";
+                                            echo "<div>";
+                                            echo "<span><label>UPORABNIŠKO IME</label></span>";
+
+                                            if($userExists){
+                                                echo "<p1>* uporabnik že obstaja!</p1>";
+                                                echo "</br>";
+                                            }
+                                            if($username==""){
+                                                echo "<span><input type='text' name='username' id='txtUsername'></span>";
+                                                echo "<p1>* vpišite uporabniško ime!</p1>";
+                                            }
+                                            else{
+                                                echo "<span><input type='text' name='username' id='txtUsername' value='$username'></span>";
+                                            }
+                                            echo "</div>";
+                                            echo "<div>";
+                                            echo "<span><label>GESLO</label></span>";
+
+
+                                            echo "<span><input type='password' name='password' id='txtPassword'></span>";
+                                            if($password==""){
+                                                echo "<p1>* vpišite geslo!</p1>";
+                                            }
+
+
+                                            echo "</div>";
+                                            echo "<div>";
+                                            echo "<span><label>EMAIL NASLOV</label></span>";
+
+                                            if($email==""){
+                                                echo "<span><input type='email' name='email' id='txtEmail'></span>";
+                                                echo "<p1>* vpišite e-poštni naslov!</p1>";
+                                            }
+                                            else{
+                                                echo "<span><input type='email' name='email' id='txtEmail' value='$email'></span>";
+                                            }
+                                            echo "</div>";
+                                            echo "<div>";
+                                            echo "<input type='submit' value='Registriraj' name='register' id='btnRegister' >";
+
+                                            echo "</br>";
+                                            echo "<p1>Opomba: za uspešno registracijo morajo biti izpolnjena vsa navedena polja</p1>";
+                                            echo "</div>";
+                                            echo "</form>";
+
+                                        }
+
+                                        else{
+                                            $sqlInsert = "INSERT INTO uporabnik (uporabnisko_ime, geslo, ime, priimek, email)
+                                                            VALUES ('$username', '$password', '$name', '$surname', '$email');";
+                                            // execute sqlInsert
+                                            $sqlSumniki = "SET NAMES 'utf8' COLLATE 'utf8_slovenian_ci';";
+                                            $sqlUTF = "SET CHARACTER SET 'utf8_slovenian_ci';";
+                                            mysql_query($sqlUTF);
+                                            mysql_query($sqlSumniki);
+                                            mysql_query($sqlInsert);
+                                            //mysqli_query($dbConn, $sqlInsert);
+                                            echo "Uporabnik $username uspešno registriran!";
+                                            echo "</br>";
+                                            echo "Čez par sekund boste preusmerjeni na stran za prijavo!";
+                                            header("refresh:5; url=index.php");
+                                        }
 									}
 								}
-								if (!isset($_POST['register']) || $userExists){
+								if (!isset($_POST['register'])){
 									echo "<h3>Registracija uporabnika</h3>";
 										echo "<form id='registerForm' name='registerForm' action='registriraj.php' method='POST'>";
 											echo "<div>";
@@ -123,13 +212,69 @@
 												echo "<span><label>EMAIL NASLOV</label></span>";
 												echo "<span><input type='email' name='email' id='txtEmail'></span>";
 											echo "</div>";
-										   echo "<div>";
-												echo "<input type='submit' value='Registriraj' name='register' id='btnRegister'>";
-											   echo "<p1>Opomba: za uspešno registracijo morajo biti izpolnjena vsa navedena polja</p1>";
+										    echo "<div>";
+												echo "<input type='submit' value='Registriraj' name='register' id='btnRegister' >";
+
+                                                echo "<p1>Opomba: za uspešno registracijo morajo biti izpolnjena vsa navedena polja</p1>";
+
+
 										   echo "</div>";
 										echo "</form>";
-		
-								}			
+								}
+                                /*
+                                else{
+                                    echo "<h3>Registracija uporabnika</h3>";
+                                    echo "<form id='registerForm' name='registerForm' action='registriraj.php' method='POST'>";
+                                    echo "<div>";
+                                    echo "<span><label>IME</label></span>";
+                                    echo "<span><input type='text' name='name' id='txtName'><span>";
+                                    if($name==""){
+                                        echo "<p1>* vpišite ime!</p1>";
+                                    }
+
+                                    echo "</div>";
+                                    echo "<div>";
+                                    echo "<span><label>PRIIMEK</label></span>";
+                                    echo "<span><input type='text' name='surname' id='txtSurname'></span>";
+                                    if($surname==""){
+                                        echo "<p1>* vpišite priimek!</p1>";
+                                    }
+                                    echo "</div>";
+                                    echo "<div>";
+                                    echo "<span><label>UPORABNIŠKO IME</label></span>";
+                                    echo "<span><input type='text' name='username' id='txtUsername'></span>";
+                                    if($userExists){
+                                        echo "<p1>* uporabnik že obstaja!</p1>";
+                                        echo "</br>";
+                                    }
+                                    if($username==""){
+
+                                        echo "<p1>* vpišite uporabniško ime!</p1>";
+                                    }
+                                    echo "</div>";
+                                    echo "<div>";
+                                    echo "<span><label>GESLO</label></span>";
+                                    echo "<span><input type='password' name='password' id='txtPassword'></span>";
+                                    if($password==""){
+                                        echo "<p1>* vpišite geslo!</p1>";
+                                    }
+                                    echo "</div>";
+                                    echo "<div>";
+                                    echo "<span><label>EMAIL NASLOV</label></span>";
+                                    echo "<span><input type='email' name='email' id='txtEmail'></span>";
+                                    if($email==""){
+                                        echo "<p1>* vpišite e-poštni naslov!</p1>";
+                                    }
+                                    echo "</div>";
+                                    echo "<div>";
+                                    echo "<input type='submit' value='Registriraj' name='register' id='btnRegister' >";
+
+                                    echo "</br>";
+                                    echo "<p1>Opomba: za uspešno registracijo morajo biti izpolnjena vsa navedena polja</p1>";
+                                    echo "</div>";
+                                    echo "</form>";
+                                }
+                                */
 							?>
 				    		</div>
   						</div>				
