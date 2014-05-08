b655f9c0effa63bb46965a6d2b13e9afd9309a07
z-a-x <plut.jaka@gmail.com>
2014-05-08 20:50:14 +0200

    Popravljeni izvajalci

--- izvajalci.php
+++ izvajalci.php
@@ -21,58 +21,53 @@ include "scripts/connect_to_mysql.php";
             <div class="cartires-grids">
 
                 <?php
-                $sql = "SELECT * FROM album ORDER BY id_izvajalec DESC LIMIT 10";
+                    $sql = "SELECT izvajalec.id_izvajalec, izvajalec.ime, album.opis, album.cena, album.image, album.id_izvajalec, album.naslov
+                            FROM izvajalec
+                            INNER JOIN album
+                            ON izvajalec.id_izvajalec=album.id_izvajalec
+                            ORDER BY izvajalec.ime
+                            LIMIT 10;";
 
+                    $result = mysql_query($sql);
+                    //$result1 = mysql_query($sql1);
+                    if (!$result) {
+                        echo "Could not successfully run query ($sql) from DB: " . mysql_error();
+                        exit;
+                    }
 
-                $result = mysql_query($sql);
-                //$result1 = mysql_query($sql1);
-                if (!$result) {
-                    echo "Could not successfully run query ($sql) from DB: " . mysql_error();
-                    exit;
-                }
+                    if (mysql_num_rows($result) == 0) {
+                        echo "No rows found, nothing to print so am exiting";
+                        exit;
+                    }
 
-                if (mysql_num_rows($result) == 0) {
-                    echo "No rows found, nothing to print so am exiting";
-                    exit;
-                }
-
-                while ($row = mysql_fetch_assoc($result) ){
-
-                    echo '<div class="cartire-grid">';
-                    echo '<div class="cartire-grid-img">';
-                    echo '<img src="album_images/'.$row["image"].'" title="tries" />';
-                    echo '</div>';
-
-                    echo '<div class="cartire-grid-info">';
-                    echo '<ul>';
-                    echo '<li><span>Novo</span>|&nbsp;&nbsp;na voljo !</li>';
-                    echo '</ul>';
-
-
-
-                    echo '<h3>'.$row["naslov"].'</h3>';
-                    echo '<p>'.$row["opis"].'</p>';
-                    echo '</div>';
-                    echo '<div class="cartire-grid-cartinfo">';
-                    echo '<h4>Cena!</h4>';
-                    echo '<span>$502.00</span><br><br>';
-                    echo '<a href="album.php">Dodaj v košarico</a><br />';
-                    echo '<a href="album.php">Več info</a>';
-                    echo '</div>';
-                    echo '<div class="clear"> </div>';
-                    echo '</div><br />';
-
-                }
+                    while ($row = mysql_fetch_assoc($result) ){
+                        echo '<div class="cartire-grid">';
+                            echo '<div class="cartire-grid-img">';
+                                echo '<img src="album_images/'.$row["image"].'" title="tries" />';
+                            echo '</div>';
+                        echo '<div class="cartire-grid-info">';
+                            echo '<ul>';
+                                echo '<li><span>Novo</span>|&nbsp;&nbsp;na voljo !</li>';
+                            echo '</ul>';
+                            echo '<h4>'.$row["ime"].'</h4>';
+                            echo '<h2>'.$row["naslov"].'</h2s>';
+                            echo '<p>'.$row["opis"].'</p>';
+                        echo '</div>';
+                        echo '<div class="cartire-grid-cartinfo">';
+                            echo '<h4>Cena!</h4>';
+                            echo '<span>'.$row["cena"].' €'.'</span><br><br>';
+                            echo '<a href="album.php">Dodaj v košarico</a><br />';
+                            echo '<a href="album.php">Več info</a>';
+                        echo '</div>';
+                        echo '<div class="clear"> </div>';
+                        echo '</div><br/>';
+                    }
                 ?>
 
 
-
-
-
-
-
             </div>
-        </div> <!--End-cartires-page---->
+        </div>
+        <!--End-cartires-page---->
     </div>
     <div class="clear"> </div>
     <?php include "footer.php";?>  <!-- Footer -->
